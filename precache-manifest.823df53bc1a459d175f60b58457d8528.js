63e7cc04afa776311c0e3bdf869ea65688e8ebc2
sujin22 <aoghksj@naver.com>
2020-11-11 17:56:30 +0900

    Updates

---
+++ precache-manifest.823df53bc1a459d175f60b58457d8528.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f9c2cf92c04aa420607639e05e1bb70",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "375bbbb5f373747a6d22",
+    "url": "/movie_app_2020/static/css/main.f875c9a5.chunk.css"
+  },
+  {
+    "revision": "e2db8dbeff0daf97e999",
+    "url": "/movie_app_2020/static/js/2.3c0d2268.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.3c0d2268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "375bbbb5f373747a6d22",
+    "url": "/movie_app_2020/static/js/main.a0675531.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
